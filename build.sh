17bf537a0f0c8263a33d098a4076887eb424196a
Jose Luis Duran <jlduran@users.noreply.github.com>
2024-10-21 00:44:58 +0000

    Minor improvements

--- build.sh
+++ build.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 set -ex
 
+# TODO
+# - parse args
+
 # Use curl, fetch or wget to DOWNLOAD
 CURL="curl -s"      # macOS
 FETCH="fetch -qo -" # FreeBSD
 WGET="wget -qO -"   # Linux (mostly)
 
+# Print an error message and exit
+# $*: error message
 err() {
 	echo "Error: $*" >&2
 	exit 1
 }
-
-# TODO parse args
-# - arch
-# - freebsd_version
-# - latest?
 
 # Check prerequisites
 check_prerequisites()
@@ -58,8 +58,19 @@ check_downloader()
 	fi
 }
 
+# Get the appropriate architechture name
+# $1: machine type
+get_arch()
+{
+	case "$1" in
+		amd64) echo amd64 ;;
+		arm64) echo aarch64 ;;
+		*) err "Machine type $(uname -m) not supported" ;;
+	esac
+}
 
 # XXX Remove stuff we don't need
+# $1: base root directory
 prune_base()
 {
 	base_root="$1"
@@ -88,7 +99,16 @@ prune_base()
 	rm -rf "${base_root:?}/var/yp/*"
 }
 
-# Build container from scratch
+# Download FreeBSD base
+# XXX multi-arch
+tmp_dir=$(mktemp -d /tmp/base.XXXXXXX)
+check_downloader
+cpu="arm64"
+arch="$(get_arch $cpu)"
+version="15.0-CURRENT"
+$DOWNLOAD "https://download.freebsd.org/snapshots/${cpu}/${arch}/${version}/base.txz" > "${tmp_dir}/base.txz"
+
+# Build the container from scratch
 oci_container_id=$(buildah from scratch)
 if [ -z "$oci_container_id" ]; then
 	err "Invalid OCI container ID"
@@ -100,19 +120,16 @@ if [ -z "$base_root" ]; then
 	err "Unable to mount the OCI container"
 fi
 
-# Download FreeBSD base
-# XXX multi-arch
-tmp_dir=$(mktemp -d /tmp/base.XXXXXXX)
-check_downloader
-$DOWNLOAD https://download.freebsd.org/snapshots/arm64/aarch64/15.0-CURRENT/base.txz > "${tmp_dir}/base.txz"
+# Untar base to the mounted container
 tar -xvf "${tmp_dir}/base.txz" -C "$base_root"
-rm -fr "$tmp_dir"
 
-# Use termcap.small
+# Prepare the container
 cp -p "${base_root}/etc/termcap.small" "${base_root}/usr/share/misc/termcap"
-
 prune_base "$base_root"
 
 # Unmount and commit the container
 buildah unmount "$oci_container_id"
-buildah commit --rm "$oci_container_id" freebsd:15.0
+buildah commit --rm "$oci_container_id" freebsd:${version}
+
+# Cleanup
+rm -fr "$tmp_dir"
